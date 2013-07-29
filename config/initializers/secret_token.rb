89e18a1d2fa2b896394ac940aa319e814c3e7bee
Marcelo G. Cajueiro <marcelocajueiro@gmail.com>
2013-07-29 19:30:46 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Solutio::Application.config.secret_key_base = '7f0ec6a24e66bcd4012e03cdef0f3bdaa155eec32182cb32b7cf80cf03b87022c8c523226ae5a3c7ee24e01283b60237a045646ce0032df30cc8ddfd157a169d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Solutio::Application.config.session_store :cookie_store, key: '_solutio_session'
