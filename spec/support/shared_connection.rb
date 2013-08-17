c9fc3774a8c0d47fab01e3f204c21f38e715b4f2
Marcelo G. Cajueiro <marcelocajueiro@gmail.com>
2013-08-16 22:18:14 -0300

    Add shared_connection configuration to the specs
    Link to why:
      http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/

---
+++ spec/support/shared_connection.rb
@@ -0,0 +1,14 @@
+# Clean the database without use DatabaseCleaner using transactional fixtures
+#
+# Source:
+#   http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
