3996235874ebac8bd6a5666faa3d5d3a426cdac9
Breno Moura <basmoura@gmail.com>
2013-10-10 00:25:34 -0300

    Create migration and modify schema inserting user provider and uid for facebook authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130817131335) do
+ActiveRecord::Schema.define(version: 20131009014658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20130817131335) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
