6723275da20e10372684d50be20bf8e088b6f509
Breno Moura <basmoura@gmail.com>
2013-10-10 00:24:12 -0300

    Configure devise model for facebook authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  require "omniauth-facebook"
+
+  config.omniauth :facebook, "244719482343728", "aeb10ad3912d1450c2866c59ee6aa24b"
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
